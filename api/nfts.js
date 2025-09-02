d20f358c8731009420100d4a6a48891669c128fd
Saeid <sparksaeid@gmail.com>
2025-09-02 22:08:19 +0330

    .

--- api/nfts.js
+++ api/nfts.js
@@ -1,17 +1,28 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
-  const { id } = req.query;
+  try {
+    const { id } = req.query;
 
-  const db = JSON.parse(fs.readFileSync("db.json", "utf-8"));
-  const nft = db.nfts.find((item) => item.id.toString() === id);
+    const dbPath = path.join(__dirname, "..", "db.json");
+    const db = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+    const nft = db.nfts.find((item) => item.id.toString() === id);
 
-  if (!nft) {
-    res.status(404).json({ error: "NFT not found" });
-    return;
+    if (!nft) {
+      res.status(404).json({ error: "NFT not found" });
+      return;
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(nft);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server crashed", details: err.message });
   }
-
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(nft);
 }
